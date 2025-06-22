df9ff9d16336e6ca2982927c98ffbe2bfc6b9e25
khaoua maryem <maryem.khaoua@gmail.com>
2025-06-22 22:46:20 +0100

    first commit

---
+++ src/main/java/org/example/codegardenbackend/CodegardenBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.codegardenbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodegardenBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodegardenBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/codegardenbackend/CodegardenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.codegardenbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodegardenBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
